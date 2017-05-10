1b5111230201619e3805664d986b76f5a47d4ee6
meagdoh <meagdoherty@gmail.com>
2017-05-10 09:02:26 -0400

    Homework

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+  .module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("imagesIndex", {
+    url: "/images",
+    templateUrl: "js/ng-views/index.html",
+    controller: "ImageIndexController",
+    controllerAs: "vm"
+  })
+  .state("imageShow", {
+    url: "/image/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "ImageShowController",
+    controllerAs: "vm"
+  })
+}
 })();
